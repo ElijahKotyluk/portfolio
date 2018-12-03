91a408a7182786b71ab62352e38f6467381b805a
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-12-03 00:02:39 -0800

    Create projects data object to dynamically display project list. Remove navigation sidebar. Add navigation items to toolbar.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,61 +1,40 @@
 <template>
   <v-app dark>
-
-    <v-navigation-drawer
-    :mini-variant="miniVariant"
-    :clipped="clipped"
-    width="200"
-    hide-overlay
-    v-model="drawer"
-    fixed
-    app
-    >
-      <v-list>
-        <v-list-tile
-        v-for="(item, i) in items"
-        :to="item.to"
-        :key="i"
-        router
-        exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon" />
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-
-
     <v-toolbar
       :clipped-left="clipped"
       fixed
       app
     >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
       <v-toolbar-title v-text="title.name" :href="title.href" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-        class="hidden-md-and-down"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
       <v-spacer></v-spacer>
       <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
+      v-for="(item, i) in items"
+      :to="item.to"
+      :key="i"
+      router
+      exact
       >
-        <v-icon>folder_open</v-icon>
+        <v-list-tile-action>
+          <v-icon v-html="item.icon" />
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title v-text="item.title" />
+        </v-list-tile-content>
       </v-btn>
+
+
+      <v-btn
+      @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-list-tile-action>
+          <v-icon>folder_open</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title v-text="projects.title" />
+        </v-list-tile-content>
+      </v-btn>
+
+
     </v-toolbar>
 
 
@@ -84,7 +63,7 @@
 
       <v-list>
         <v-list-tile
-          v-for="(project, i) in projects"
+          v-for="(project, i) in projects.list"
           :key="i"
           :href="project.href"
         >
@@ -104,7 +83,9 @@
       :fixed="fixed"
       app
     >
+      <v-spacer></v-spacer>
       <span>&copy; Elijah Kotyluk 2018</span>
+      <v-spacer></v-spacer>
     </v-footer>
   </v-app>
 </template>
@@ -122,11 +103,14 @@
           { icon: 'bubble_chart', title: 'Skills', to: '/skills' },
           { icon: 'email', title: 'Contact', to: '/contact'}
         ],
-        projects: [
-          { icon: 'today', title: 'vue-moment-calendar', href: 'https://github.com/ElijahKotyluk/Vue-Moment-Calendar'},
-          { icon: 'search', title: 'nyt-react-search', href: 'https://github.com/ElijahKotyluk/NYT-react-search'},
-          { icon: 'chat', title: 'vue-firebase-chat', href: 'https://github.com/ElijahKotyluk/vue-firebase-chat'}
-        ],
+        projects: {
+          title: 'Projects',
+          list: [
+            { icon: 'today', title: 'vue-moment-calendar', href: 'https://github.com/ElijahKotyluk/Vue-Moment-Calendar'},
+            { icon: 'search', title: 'nyt-react-search', href: 'https://github.com/ElijahKotyluk/NYT-react-search'},
+            { icon: 'chat', title: 'vue-firebase-chat', href: 'https://github.com/ElijahKotyluk/vue-firebase-chat'}
+          ]
+        },
         miniVariant: false,
         right: true,
         rightDrawer: false,
