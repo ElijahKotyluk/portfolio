b985becc28b5045d72e41605210ecef22630749c
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-12-30 06:34:20 -0800

    Remove v-card. Add zoom in animation to v-avatar.

--- pages/index.vue
+++ pages/index.vue
@@ -8,86 +8,44 @@
       sm8
       md6>
 
-      <v-container>
+      <v-container
+      column
+      justify-space-around
+      align-content-space-between>
+
         <!-- avatar -->
         <v-avatar
-        class="elevation-12"
+        class="elevation-12 img-zoom"
         size="200">
           <img src="../static/mee.png" alt="avatar" />
         </v-avatar>
+
+        <div class="text-xs-center">
+          <p class="display-1 pt-3">
+            Elijah Kotyluk
+          </p>
+        </div>
 
         <!-- social links -->
         <div class="text-xs-center">
           <a
           href="https://www.linkedin.com/in/elijahkotyluk"
           target="_blank">
-            <fa :icon="['fab', 'linkedin']" style="font-size: 25px" />
+            <fa :icon="['fab', 'linkedin']" style="font-size: 40px" />
           </a>
           <a
           href="https://twitter.com/ElijahDavidK"
           target="_blank">
-            <fa :icon="['fab', 'twitter']" style="font-size: 25px" />
+            <fa :icon="['fab', 'twitter']" style="font-size: 40px" />
           </a>
           <a
           href="https://github.com/ElijahKotyluk"
           target="_blank">
-            <fa :icon="['fab', 'github']" style="font-size: 25px"/>
+            <fa :icon="['fab', 'github']" style="font-size: 40px"/>
           </a>
         </div>
       </v-container>
 
- <!--
-      <v-card
-      class="mt-5 elevation-12"
-      width="50vh">
-        <v-toolbar>
-          <v-spacer />
-          <v-card-title class="headline">
-            Elijah Kotyluk
-          </v-card-title>
-          <v-spacer />
-        </v-toolbar>
-        <v-card-title class="headline justify-center mt-3">
-        </v-card-title>
-        <v-card-text>
-          <div class="text-xs-center">
-            <p>Hello, I'm a self-taught software developer in his 20's.</p>
-            <p>Want to check out what I'm working on? <a
-              href="https://github.com/ElijahKotyluk"
-              target="_blank">github</a>.</p>
-          </div>
-          <div class="text-xs-right">
-            <em><small>&mdash; Elijah Kotyluk</small></em>
-          </div>
-          <hr class="my-3">
-        </v-card-text>
-        <v-card-actions>
-          <div class="text-xs-center">
-            <a
-            href="https://www.linkedin.com/in/elijahkotyluk"
-            target="_blank">
-              <fa :icon="['fab', 'linkedin']" style="font-size: 25px" />
-            </a>
-            <a
-            href="https://twitter.com/ElijahDavidK"
-            target="_blank">
-              <fa :icon="['fab', 'twitter']" style="font-size: 25px" />
-            </a>
-            <a
-            href="https://github.com/ElijahKotyluk"
-            target="_blank">
-              <fa :icon="['fab', 'github']" style="font-size: 25px"/>
-            </a>
-          </div>
-          <v-spacer/>
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/skills">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
-    -->
       <div class="text-xs-center mt-5">
         <h3 class="ma-3">This site is powered by:</h3>
         <logo/>
@@ -108,3 +66,21 @@ export default {
   }
 }
 </script>
+
+<style>
+@keyframes zoom-in-out {
+    0% {
+        transform: scale(.3,.3);
+    }
+    50% {
+        transform: scale(1.1,1.1);
+    }
+    100% {
+        transform: scale(1,1);
+    }
+}
+
+.img-zoom {
+	animation: zoom-in-out 1s;
+}
+</style>
