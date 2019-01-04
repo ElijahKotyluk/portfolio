bd30db3dd3833563423da098a18ec379f7603f76
ElijahKotyluk <elijah@elijahkotyluk.com>
2019-01-04 08:00:36 -0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,58 +2,83 @@
 
 
       <!-- Intro Container -->
-      <v-container fill-height>
+      <v-container
+      fluid>
         <v-layout
-        column
-        align-center>
-          <!-- Avatar -->
-          <v-avatar
-          class="elevation-12 intro-zoom"
-          size="256">
-            <img src="../static/mee.png" alt="avatar" />
-          </v-avatar>
+        column>
+          <v-flex xs12 sm24 md12
+          class="pb-3">
+            <!-- Avatar -->
+            <v-layout column align-center>
+              <v-avatar
+              class="elevation-12 intro-zoom"
+              size="256">
+                <img src="../static/mee.png" alt="avatar" />
+              </v-avatar>
+            </v-layout>
 
-          <!-- Intro Name/Title -->
-          <div class="text-xs-center intro-zoom pt-2">
-            <p class="display-1 pt-3">
-              Elijah Kotyluk
-            </p>
-          </div>
+            <!-- Intro Name/Title -->
+            <div class="text-xs-center intro-zoom pt-2">
+              <p class="display-1 pt-3">
+                Elijah Kotyluk
+              </p>
+            </div>
 
-          <!-- Social Links -->
-          <div class="text-xs-center intro-zoom">
-            <a
-            class="pa-2"
-            href="https://www.linkedin.com/in/elijahkotyluk"
-            target="_blank">
-              <fa :icon="['fab', 'linkedin']" style="font-size: 40px" />
-            </a>
-            <a
-            class="pa-2"
-            href="https://twitter.com/ElijahDavidK"
-            target="_blank">
-              <fa :icon="['fab', 'twitter']" style="font-size: 40px" />
-            </a>
-            <a
-            class="pa-2"
-            href="https://github.com/ElijahKotyluk"
-            target="_blank">
-              <fa :icon="['fab', 'github']" style="font-size: 40px"/>
-            </a>
-          </div>
+            <!-- Social Links -->
+            <div class="text-xs-center intro-zoom">
+              <a
+              class="pa-2"
+              href="https://www.linkedin.com/in/elijahkotyluk"
+              target="_blank">
+                <fa :icon="['fab', 'linkedin']" style="font-size: 40px" />
+              </a>
+              <a
+              class="pa-2"
+              href="https://twitter.com/ElijahDavidK"
+              target="_blank">
+                <fa :icon="['fab', 'twitter']" style="font-size: 40px" />
+              </a>
+              <a
+              class="pa-2"
+              href="https://github.com/ElijahKotyluk"
+              target="_blank">
+                <fa :icon="['fab', 'gitkraken']" style="font-size: 40px"/>
+              </a>
+            </div>
+          </v-flex>
+
+          <!-- Divider -->
+          <v-divider></v-divider>
+
+          <!-- About Me -->
+          <v-container fluid>
+            <v-flex xs12 sm24 md12>
+              <about-me></about-me>
+            </v-flex>
+          </v-container>
+
+          <!-- Divider -->
+          <v-divider></v-divider>
+
+          <!-- Skills -->
+          <v-flex xs12 sm24 md12>
+            <skills></skills>
+          </v-flex>
         </v-layout>
-      </v-container fluid>
+      </v-container>
 </template>
 
 <script>
 import Logo from '~/components/animations/Logo.vue'
 import VuetifyLogo from '~/components/animations/VuetifyLogo.vue'
+import AboutMe from '~/components/sections/AboutMe.vue'
 import Skills from '~/components/sections/Skills.vue'
 
 export default {
   components: {
     Logo,
     VuetifyLogo,
+    AboutMe,
     Skills
   }
 }
