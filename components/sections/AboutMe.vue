1cb571f613eb281fd4f89b30369ee04e6f93cef9
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-01-05 15:11:48 -0800

    AboutMe

---
+++ components/sections/AboutMe.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container fluid>
+    <v-layout
+      column
+      justify-center
+      align-center>
+      <v-flex xs12 sm8 md6>
+        <p class="display-2">
+          Hello
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'AboutMe',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
